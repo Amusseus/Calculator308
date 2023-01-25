987b1c97ee2c57890e41bdf86a88cc4cd44688d7
jwu205 <jinwu2003@gmail.com>
2023-01-25 15:28:41 -0800

    Base Calculator Code

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
 
+public class Calculator extends JFrame {
 
-public class Calculator {
+    public Calculator() {
+        super("Calculator");
+        GridLayout layout = new GridLayout();
+        setLayout(layout);
+        JTextField text = new JTextField("");
+        add(text);
+        JPanel panel = new JPanel();
+        GridLayout panelLayout = new GridLayout(4,4);
+        panel.setLayout(panelLayout);
+        JButton buttons[] = new JButton [16];
+        for(int i = 0; i < 16; i++){
+            buttons[i] = new JButton(i + "");
+            panel.add(buttons[i]);
+        }
+        add(panel);
 
+    }
 
-    
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calculator.setSize(500, 500);
+        calculator.setVisible(true);
+    }
 }
