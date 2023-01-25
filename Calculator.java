9cb014a341230a74d13c996ef16130884e7555e3
jwu205 <jinwu2003@gmail.com>
2023-01-25 15:47:30 -0800

    Calculator Layout

--- Calculator.java
+++ Calculator.java
@@ -3,22 +3,28 @@ import java.awt.*;
 
 public class Calculator extends JFrame {
 
+    private void createButtons(){
+        
+    }
+
     public Calculator() {
         super("Calculator");
-        GridLayout layout = new GridLayout();
+        GridLayout layout = new GridLayout(5, 1);
         setLayout(layout);
         JTextField text = new JTextField("");
         add(text);
-        JPanel panel = new JPanel();
-        GridLayout panelLayout = new GridLayout(4,4);
-        panel.setLayout(panelLayout);
-        JButton buttons[] = new JButton [16];
-        for(int i = 0; i < 16; i++){
-            buttons[i] = new JButton(i + "");
-            panel.add(buttons[i]);
-        }
-        add(panel);
 
+        for(int i = 0 ; i < 4; i++){
+            JPanel panel = new JPanel();
+            GridLayout panelLayout = new GridLayout(1,4);
+            panel.setLayout(panelLayout);
+            JButton buttons[] = new JButton [4];
+            for(int j = 0; j < 4; j++){
+                buttons[j] = new JButton(i * j + "");
+                panel.add(buttons[j]);
+            }
+            add(panel);
+        }
     }
 
     public static void main(String[] args) {
